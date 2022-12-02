5860b4886a34c06c24c690637c799743001a3947
Hung Tran Van <tvhung1>
2022-12-02 13:52:41 +0700

    Update new variable name

--- src/components/LineAnimation/index.js
+++ src/components/LineAnimation/index.js
@@ -1,24 +1,24 @@
 const LineAnimation = () => {
-	const getPathData = (lineItems, pathWidth, isActive) => {
-		lineItems = Object.values(lineItems);
+	const getPathData = (containerItems, pathWidth, isActive) => {
+		containerItems = Object.values(containerItems);
 
 		let d = '';
 		let oldY = 0;
 		const radius = parseFloat(5);
 		pathWidth /= 2;
 
-		lineItems.forEach((item, index) => {
-			if (index === 0) d += `M${pathWidth} 0`;
+		containerItems.forEach((item, index) => {
+			if (index === 0) d += `M ${pathWidth} 0`;
 
-			if (lineItems.length > 0) {
+			if (containerItems.length > 0) {
 				oldY = oldY + item.offsetHeight;
-				const top = lineItems[0].getBoundingClientRect().top;
+				const top = containerItems[0].getBoundingClientRect().top;
 				const center = window.innerHeight / 2;
 				const height = center - top;
 
-				if (index === lineItems.length - 1) {
+				if (index === containerItems.length - 1) {
 					if (isActive && oldY > height) return (d += `V ${height}`);
-					else d += `V${oldY + 140}`;
+					else d += `V ${oldY + 140}`;
 				} else {
 					const itemWidth = item.offsetWidth;
 
@@ -30,7 +30,7 @@ const LineAnimation = () => {
 					let c5 = index % 2 === 0 ? itemWidth - pathWidth - radius : pathWidth + radius;
 					let c6 = index % 2 === 0 ? itemWidth - pathWidth : pathWidth;
 
-					if (index === lineItems.length - 2) {
+					if (index === containerItems.length - 2) {
 						c4 = itemWidth / 2;
 						c5 = index % 2 === 0 ? (itemWidth - 2) / 2 + radius : (itemWidth - 2) / 2 - radius;
 						c6 = index % 2 === 0 ? (itemWidth - 2) / 2 + radius * 2 : (itemWidth - 2) / 2 - radius * 2;
@@ -51,22 +51,22 @@ const LineAnimation = () => {
 		return d;
 	};
 
-	const initChild = item => {
-		let lineItems = item.getElementsByClassName('itm-line-item');
-		const itemRect = item.getBoundingClientRect();
-		const svg = item.getElementsByClassName('itm-line-svg')[0];
+	const initChild = lineItem => {
+		let containerItems = lineItem.getElementsByClassName('itm-line-container');
+		const lineItemRect = lineItem.getBoundingClientRect();
+		const svg = lineItem.getElementsByClassName('itm-line-svg')[0];
 
-		const path = item.getElementsByClassName('itm-line-path')[0];
+		const path = lineItem.getElementsByClassName('itm-line-path')[0];
 		const pathWidth = path.getAttribute('stroke-width');
 
-		const pathActive = item.getElementsByClassName('itm-line-path-active')[0];
+		const pathActive = lineItem.getElementsByClassName('itm-line-path-active')[0];
 		const pathActiveWidth = parseFloat(pathActive.getAttribute('stroke-width'));
 		const pathMaxWidth = pathWidth > pathActiveWidth ? pathWidth : pathActiveWidth;
 
-		const dPath = getPathData(lineItems, pathMaxWidth);
-		const dPathActive = getPathData(lineItems, pathMaxWidth, true);
+		const dPath = getPathData(containerItems, pathMaxWidth);
+		const dPathActive = getPathData(containerItems, pathMaxWidth, true);
 
-		svg.setAttribute('viewBox', `0 0 ${itemRect.width} ${itemRect.height}`);
+		svg.setAttribute('viewBox', `0 0 ${lineItemRect.width} ${lineItemRect.height}`);
 		path.setAttribute('d', dPath);
 		pathActive.setAttribute('d', dPathActive);
 	};
@@ -75,8 +75,8 @@ const LineAnimation = () => {
 		let items = document.getElementsByClassName('itm-line');
 		items = Object.values(items);
 
-		items.forEach(item => {
-			initChild(item);
+		items.forEach(lineItem => {
+			initChild(lineItem);
 		});
 	};
 
